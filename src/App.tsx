10f1b780acb036b7d229735df2d8d6583822c539
Zahra <bghzahra@outlook.com>
2022-03-16 08:50:50 +0000

    created random dog API

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,52 @@
 import { useState } from "react";
 
-interface Joke {
-  id: number;
-  type: string;
-  setup: string;
-  punchline: string;
+interface Dog {
+  message: string;
+  status: string;
 }
 
 function App() {
-  const [joke, setJoke] = useState<Joke>();
+  const [dog, setDog] = useState<Dog>();
 
-  const handleGetJoke = async () => {
+  const handleGetdog = async () => {
     const response = await fetch(
-      "https://jokestemp.neillbogie.repl.co/jokes/general/random"
+      "https://dog.ceo/api/breeds/image/random"
     );
-    const jsonBody: Joke[] = await response.json();
-    setJoke(jsonBody[0]);
+    const jsonBody: Dog = await response.json();
+    setDog(jsonBody);
+    console.log(jsonBody.message, jsonBody.status)
   };
 
-  // const handleGetJoke = () => {
-  //   fetch("https://jokestemp.neillbogie.repl.co/jokes/general/random")
+  // const handleGetdog = () => {
+  //   fetch("https://dog.ceo/api/breeds/image/random")
   //     .then((response) => response.json())
-  //     .then((jsonBody: Joke[]) => setJoke(jsonBody[0]));
+  //     .then((jsonBody: Dog) => setDog(jsonBody));
   // };
 
-  if (joke) {
+  if (dog) {
     return (
       <div>
-        <h1>Joke app</h1>
+        <h1>dog app</h1>
+        <img src={dog.message} alt="random Dog " />s
         <details>
-          <summary>{joke.setup}</summary>
-          <p>{joke.punchline}</p>
+          <summary>{dog.message}</summary>
+          <p>{dog.status}</p>
         </details>
         <hr />
-        <button onClick={handleGetJoke}>Get another joke</button>
+        <button onClick={handleGetdog}>Get another dog</button>
+       
       </div>
     );
   } else {
     return (
       <div>
-        <h1>Joke app</h1>
+        <h1>dog app</h1>
         <p>
           Click the button to trigger a <code>fetch</code> that gets a random
-          joke from an API!
+          dog from an API!
         </p>
-        <button onClick={handleGetJoke}>Get joke</button>
+        
+        <button onClick={handleGetdog}>Get dog</button>
       </div>
     );
   }
